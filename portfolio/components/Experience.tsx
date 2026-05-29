d1e1b9280b8ad77e262e37eda19fb76f242a2af0
Issar Manknojiya <imanknoj@uwo.ca>
2026-05-29 14:25:34 -0400

    update experience section content

--- portfolio/components/Experience.tsx
+++ portfolio/components/Experience.tsx
@@ -6,12 +6,12 @@ import { FiArrowDown } from "react-icons/fi";
 
 const experiences = [
     {
-        role: 'Machine Learning Engineer',
-        company: 'Western Cyber Society',
-        duration: 'Nov 2025 – Present',
-        website: 'https://www.westerncybersociety.ca/',
+        role: 'Data Operations Intern',
+        company: 'SmartOne.ai',
+        duration: 'May 2026 – Present',
+        website: 'https://smartone.ai/',
         description: [
-            'Creating a pipeline of impersonation/intention nlp models to detect email phishing '
+            'Working on a robotics data collection & annotation project'
         ],
     },
     {
